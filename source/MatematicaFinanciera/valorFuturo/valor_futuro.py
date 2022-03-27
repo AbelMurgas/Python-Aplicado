4449b1ba0487c6eb368e3eb17478d55816c56748
Abel Murgas <abel.murgas@mibus.com.pa>
2022-03-27 09:31:31 -0500

    creacion de la clase valor futuro

--- source/MatematicaFinanciera/valorFuturo/valor_futuro.py
+++ source/MatematicaFinanciera/valorFuturo/valor_futuro.py
@@ -1,2 +1,7 @@
 class Valor_Futuro:
-    pass
+    
+    def obtener_valor_futuro(tiempo_anual: float, periodo: int, interes_porcentual: float, valor_presente=float):
+        i = (interes_porcentual/100)/periodo
+        n = tiempo_anual*periodo
+        resultado = valor_presente*(1+i)**n
+        return round(resultado,2)
